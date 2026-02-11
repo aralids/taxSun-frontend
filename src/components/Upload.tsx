19b177200ad2444171c51d4b3e134879d10f5b00
Dilara Sarach <dilara.sarach@abv.bg>
2026-02-11 13:58:32 +0100

    Condensed upload components into Upload.tsx, viewing section components into ViewingModes.tsx

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,25 +1,169 @@
-import { useMemo } from "react";
-import UploadTsv from "./UploadTsv.tsx";
-import UploadFaa from "./UploadFaa.tsx";
+import { useContext } from "react";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { RightSectionCtx } from "../contexts/RightSectionCtx";
 
 const Upload = () => {
-	return useMemo(() => {
-		return (
-			<Card
-				className="mt-3"
-				bg={"light"}
-				text={"black"}
-				style={{ width: "18vw" }}
-			>
-				<Card.Header>Load your data:</Card.Header>
-				<Card.Body>
-					<UploadTsv />
-					<UploadFaa />
-				</Card.Body>
-			</Card>
-		);
-	}, []);
+	const ctx: any = useContext(RightSectionCtx);
+
+	const hasTriedTsv = ctx["tsvLastTry"] !== "";
+	const hasTriedFaa = ctx["faaLastTry"] !== "";
+
+	return (
+		<Card
+			className="mt-3"
+			bg={"light"}
+			text={"black"}
+			style={{ width: "18vw" }}
+		>
+			<Card.Header>Load your data:</Card.Header>
+			<Card.Body>
+				{/* TSV */}
+				{!hasTriedTsv ? (
+					<div className="d-flex align-items-center justify-content-center w-100">
+						<Button
+							className="w-100 p-2 m-0 border-0"
+							style={{ transition: "all ease-in-out 1s" }}
+						>
+							<label
+								htmlFor="tsv-file-input"
+								className="w-100 d-flex justify-content-center"
+								style={{ cursor: "pointer" }}
+							>
+								<span className="material-symbols-outlined">upload</span>
+								TSV
+							</label>
+						</Button>
+
+						<input
+							id="tsv-file-input"
+							type="file"
+							className="d-none"
+							onChange={ctx["uplTsvHandleChange"]}
+							ref={ctx["tsvFormRef"]}
+						/>
+					</div>
+				) : (
+					<div className="d-flex align-items-center justify-content-between w-100">
+						<Button
+							className="p-2 m-0 border-0"
+							style={{
+								width: "20%",
+								minWidth: "fit-content",
+								transition: "all ease-in-out 1s",
+							}}
+						>
+							<label
+								htmlFor="tsv-file-input"
+								className="w-100 d-flex justify-content-center"
+								style={{ cursor: "pointer" }}
+							>
+								<span className="material-symbols-outlined">upload</span>
+								TSV
+							</label>
+						</Button>
+
+						<input
+							id="tsv-file-input"
+							type="file"
+							className="d-none"
+							onChange={ctx["uplTsvHandleChange"]}
+							ref={ctx["tsvFormRef"]}
+						/>
+
+						<p
+							style={{
+								textAlign: "center",
+								textOverflow: "ellipsis",
+								overflow: "hidden",
+								whiteSpace: "nowrap",
+							}}
+							className="ps-2 pe-2 m-0"
+						>
+							{ctx["tsvLastTry"]}
+						</p>
+
+						<span className="material-symbols-outlined d-inline">
+							{ctx["tsvLoadStatus"]}
+						</span>
+					</div>
+				)}
+
+				{/* FAA */}
+				{!hasTriedFaa ? (
+					<div className="d-flex align-items-center justify-content-center w-100 mt-2">
+						<Button
+							className="w-100 p-2 m-0 border-0"
+							disabled={!ctx["fastaEnabled"]}
+							style={{ transition: "all ease-in-out 1s" }}
+						>
+							<label
+								htmlFor="faa-file-input"
+								className="w-100 d-flex justify-content-center"
+								style={{ cursor: "pointer" }}
+							>
+								<span className="material-symbols-outlined">upload</span>
+								FAA
+							</label>
+						</Button>
+
+						<input
+							id="faa-file-input"
+							type="file"
+							className="d-none"
+							onChange={ctx["uplFaaHandleChange"]}
+							ref={ctx["faaFormRef"]}
+						/>
+					</div>
+				) : (
+					<div className="d-flex align-items-center justify-content-between w-100 mt-2">
+						<Button
+							className="p-2 m-0 border-0"
+							disabled={!ctx["fastaEnabled"]}
+							style={{
+								width: "20%",
+								minWidth: "fit-content",
+								transition: "all ease-in-out 1s",
+							}}
+						>
+							<label
+								htmlFor="faa-file-input"
+								className="w-100 d-flex justify-content-center"
+								style={{ cursor: "pointer" }}
+							>
+								<span className="material-symbols-outlined">upload</span>
+								FAA
+							</label>
+						</Button>
+
+						<input
+							id="faa-file-input"
+							type="file"
+							className="d-none"
+							onChange={ctx["uplFaaHandleChange"]}
+							ref={ctx["faaFormRef"]}
+						/>
+
+						<p
+							style={{
+								textAlign: "center",
+								textOverflow: "ellipsis",
+								overflow: "hidden",
+								whiteSpace: "nowrap",
+							}}
+							className="ps-2 pe-2 m-0"
+						>
+							{ctx["faaLastTry"]}
+						</p>
+
+						<span className="material-symbols-outlined d-inline">
+							{ctx["faaLoadStatus"]}
+						</span>
+					</div>
+				)}
+			</Card.Body>
+		</Card>
+	);
 };
 
 export default Upload;
--- src/components/ViewingModes.tsx
+++ src/components/ViewingModes.tsx
@@ -1,27 +1,83 @@
-import * as React from "react";
-import Collapse from "./Collapse.tsx";
-import EValue from "./EValue.tsx";
-import Views from "./Views.tsx";
+import { useContext } from "react";
 import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import { RightSectionCtx } from "../contexts/RightSectionCtx";
 
 const ViewingModes = () => {
-	return React.useMemo(() => {
-		return (
-			<Card
-				className="mt-3"
-				bg={"light"}
-				text={"black"}
-				style={{ width: "18vw" }}
-			>
-				<Card.Header>View: </Card.Header>
-				<Card.Body>
-					<Collapse />
-					<EValue />
-					<Views />
-				</Card.Body>
-			</Card>
-		);
-	}, []);
+	const ctx: any = useContext(RightSectionCtx);
+
+	return (
+		<Card className="mt-3" bg="light" text="black" style={{ width: "18vw" }}>
+			<Card.Header>View:</Card.Header>
+			<Card.Body>
+				{/* Collapse */}
+				<Form>
+					<Form.Check
+						type="checkbox"
+						id="collapse"
+						label="Collapse"
+						onChange={ctx["collHandleChange"]}
+						checked={ctx["coll"]}
+					/>
+				</Form>
+
+				{/* EValue */}
+				<Form>
+					<Form.Check
+						type="checkbox"
+						id="eValueApplied"
+						label="Filter by e-value: "
+						onChange={ctx["eValueAppliedHandleChange"]}
+						checked={ctx["eValueApplied"]}
+						disabled={!ctx["eValueEnabled"]}
+					/>
+					<Form.Control
+						id="eValue"
+						defaultValue={1.9e-28}
+						onKeyDown={ctx["eValueHandleKeyDown"]}
+						ref={ctx["eValueRef"]}
+						disabled={!ctx["eValueEnabled"]}
+					/>
+				</Form>
+
+				{/* Views */}
+				<Form className="mt-4" onChange={ctx["viewHandleChange"]}>
+					<Form.Check
+						type="radio"
+						id="unaltered"
+						label="Unaltered"
+						ref={ctx["unalteredRef"]}
+						defaultChecked={false}
+						name="radio"
+					/>
+					<Form.Check
+						type="radio"
+						id="marriedTaxaI"
+						label="Married taxa I"
+						ref={ctx["marriedIRef"]}
+						defaultChecked={false}
+						name="radio"
+					/>
+					<Form.Check
+						type="radio"
+						id="marriedTaxaII"
+						label="Married taxa II"
+						ref={ctx["marriedIIRef"]}
+						defaultChecked={false}
+						name="radio"
+					/>
+					<Form.Check
+						type="radio"
+						id="allEqual"
+						label="All equal"
+						ref={ctx["allEqualRef"]}
+						defaultChecked={true}
+						name="radio"
+					/>
+				</Form>
+			</Card.Body>
+		</Card>
+	);
 };
 
 export default ViewingModes;
